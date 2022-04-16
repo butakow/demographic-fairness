21867423e3e7b5a8087c0fcec845998cd6abd6aa
Nic Butakow <nicholas@adastralab.com>
2022-04-15 21:45:48 -0400

    Prevent twint from prematurely stopping scraping
    Also update data and scraper script

--- scrape.py
+++ scrape.py
@@ -2,7 +2,13 @@
 Tweet scraper script
 """
 
+import os
 from twint_utils import save_tweets_as_csv
 
-search_query = {'Search': '#csed OR #csedu OR #teachcs', 'Limit': 10000, 'Output': 'tweets.csv'}
+FILENAME = 'tweets.csv'
+
+if os.path.exists(FILENAME):
+    os.remove(FILENAME)
+
+search_query = {'Search': '#csed OR #csedu OR #teachcs', 'Limit': 10000, 'Output': FILENAME}
 save_tweets_as_csv(**search_query)
